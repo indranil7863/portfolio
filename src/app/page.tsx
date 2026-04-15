16379c9c3154b74ddea908ce0a8e1f0cbca4de42
indranil <ibera866@gmail.com>
2026-04-15 09:37:11 +0530

    add footer app/page background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+import Galaxy from "@/components/Background";
+
 
 export default function Home() {
   return (
-    <h1 className="bg-white">This is Home page</h1>
+   
+    <div className="bg-black/30 h-full w-full relative">
+     <h1 className="bg-gray-900/30 text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">This is Home page</h1>
+     <div style={{ width: '100%', height: '650px', position: 'relative' }}>
+      <Galaxy />
+    </div>
+
+    </div>
   );
 }

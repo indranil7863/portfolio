e0094f9066d47a4b37076a7229acefef8d182bb8
indranil <ibera866@gmail.com>
2026-04-15 12:17:56 +0530

    add about page and change footer position

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
 function page() {
   return (
-    <div>page</div>
-  )
+    <div className="text-white mx-auto w-[70%] flex flex-col gap-8 pb-16 justify-center items-center">
+      <h1 className="text-5xl tracking-wide relative font-bold py-4 bg-linear-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
+        About Indranil
+        <span
+          className="absolute inset-x-0 bottom-0 h-0.5 
+              bg-linear-to-r from-transparent via-blue-500 to-transparent"
+        ></span>
+      </h1>
+
+      {/* content */}
+      <div className="flex justify-center items-center flex-col gap-5 text-xl border-x border-x-gray-300 rounded-2xl px-6 py-6">
+        <p className="[word-spacing:0.1rem] tracking-[2px]">
+          I am a passionate Software Engineer with a knack for building
+          full-stack web applications using modern technologies like React.js,
+          Next.js, Tailwind CSS, Node.js, PostgresSQL. My journey in tech began
+          with a curiosity for solving real-world problems through innovative
+          solutions, which evolved into a love for crafting user-centric digital
+          experiences.
+        </p>
+        <p className="[word-spacing:0.1rem] tracking-[2px]">
+          With a strong foundation in JavaScript frameworks, I focus on creating
+          scalable, efficient, and visually appealing applications. Currently, I
+          am diving deeper into backend development with Node.js and Express to
+          expand my skill set and deliver powerful, server-side solutions.
+        </p>
+        <p className="[word-spacing:0.1rem] tracking-[2px]">
+          Alongside my development work, I have solved 500+ LeetCode problems
+          and 100+ gfg problems to build a strong foundation in data structures
+          and algorithms.
+        </p>
+        <p className="[word-spacing:0.1rem] tracking-[2px]">
+          Beyond coding, I thrive in collaborative environments and enjoy
+          tackling challenging problems with creative solutions. I aim to
+          contribute to impactful projects that make a difference in users'
+          lives.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,35 @@
-
-
 function Footer() {
   return (
     <div>
-    <div className='bg-gray-900/30 absolute top-full w-full  border-t-dotted border-t border-t-gray-700 py-4 text-gray-400 flex flex-col justify-center items-center gap-3' >
+      <div className="bg-gray-900/30 w-full  border-t-dotted border-t border-t-gray-700 py-4 text-gray-400 flex flex-col justify-center items-center gap-3">
         <div className="flex flex-col items-center px-8 leading-loose lg:flex-row text-gray-600">
-          <p>©2026 <span className="lg:inline hidden pr-1">.</span></p>
-          <p>Built with <span className="text-gray-200">Next.js, reactBits/ui</span> and <span className="text-gray-200">Tailwind CSS</span> <span className="lg:inline hidden pr-1">.</span></p>
-          <p><span>Coded in </span>Vs-code and deployed with <span className="text-gray-200">Vercel</span></p>
+          <p>
+            ©2026 <span className="lg:inline hidden pr-1">.</span>
+          </p>
+          <p>
+            Built with{" "}
+            <span className="text-gray-200">Next.js, reactBits/ui</span> and{" "}
+            <span className="text-gray-200">Tailwind CSS</span>{" "}
+            <span className="lg:inline hidden pr-1">.</span>
+          </p>
+          <p>
+            <span>Coded in </span>Vs-code and deployed with{" "}
+            <span className="text-gray-200">Vercel</span>
+          </p>
         </div>
         <div className="flex flex-col lg:flex-row lg:justify-center text-gray-600 leading-loose">
-          <p>Developed by <span className="text-blue-700">Indranil Bera</span> <span className="lg:inline  hidden pr-1">.</span></p>
-          <p className="">Source code available on <span className="text-blue-700">Github</span></p>
+          <p>
+            Developed by <span className="text-blue-700">Indranil Bera</span>{" "}
+            <span className="lg:inline  hidden pr-1">.</span>
+          </p>
+          <p className="">
+            Source code available on{" "}
+            <span className="text-blue-700">Github</span>
+          </p>
         </div>
+      </div>
     </div>
-    </div>
-    
-  )
+  );
 }
 
-export default Footer
+export default Footer;
